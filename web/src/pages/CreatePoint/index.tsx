c3e2afc08bbe630a8609c134305335270807c792
Vicarious76 <matheusyanmonteiro@gmail.com>
2020-07-15 21:25:04 -0300

    updated integration of API IBGE cities and ufs

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,14 +1,75 @@
-import React from 'react'
+import React, {useEffect, useState, ChangeEvent} from 'react'
 import {Link} from 'react-router-dom';
 import {FiArrowLeft} from 'react-icons/fi';
 import {Map, TileLayer, Marker} from 'react-leaflet';
+import axios from 'axios';
+import api from '../../services/api';
 
 import './styles.css';
 
 import logo from '../../assets/logo.svg'
 
+//lembrar que no back end coloquei name ao inves de title interface que pega campos do back end
+interface Item {
+    id: number;
+    name: string;
+    image_url: string;
+}
+
+//Interfaces que pega os campos necessarios da api do ibge
+interface IBGEUFResponse {
+    sigla: string;
+}
+interface IBGECITYResponse {
+    nome: string;
+}
+
 
 const CreatePoint = () => {
+
+    //Pegando items do back end localhost 3333 estado
+    const [items, setItems] = useState<Item[]>([]);
+    //Estado para armazenar as UFS
+    const [ufs, setUfs] = useState<string[]>([]);
+    //Estado para armazenar as cidades
+    const [cities, setCities] = useState<string[]>([]);
+
+    //Estados para saber se foi alterada alguma coisa
+    const [seletedUf, setSelectedUf] = useState('0');
+
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        })
+    }, []);
+
+    // APi de Estados e cidades
+    useEffect(() => {
+        axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(response => {
+            const ufInitials = response.data.map(uf => uf.sigla);
+            
+            setUfs(ufInitials);
+        });
+    },[]); 
+
+    useEffect(() => {
+       if (seletedUf === '0'){
+           return;
+       }
+       axios.get<IBGECITYResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${seletedUf}/municipios`).then(response => {
+            const cityNames = response.data.map(city => city.nome);
+            
+            setCities(cityNames);
+        });
+    }, [seletedUf]);
+
+    function handleSelectUf(event: ChangeEvent<HTMLSelectElement>){
+       const uf = event.target.value;
+       
+       setSelectedUf(uf);
+
+    }
+
     return (
         <div id="page-create-point">
             <header>
@@ -77,8 +138,16 @@ const CreatePoint = () => {
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estados (UF)</label>
-                            <select name="uf" id="uf">
+                            <select 
+                                name="uf"
+                                id="uf"
+                                value ={seletedUf}
+                                onChange = {handleSelectUf}
+                             >
                                 <option value="0">Selecione uma UF</option>
+                                {ufs.map(uf => (
+                                   <option key = {uf} value={uf}>{uf}</option> 
+                                ))}
                             </select>
                         </div>
 
@@ -86,6 +155,9 @@ const CreatePoint = () => {
                             <label htmlFor="city">Cidade</label>
                             <select name="city" id="city">
                                 <option value="0">Selecione uma Cidade</option>
+                                {cities.map(city => (
+                                   <option key = {city} value={city}>{city}</option> 
+                                ))}
                             </select>
                         </div>
                     </div>
@@ -99,35 +171,14 @@ const CreatePoint = () => {
                     </legend>
 
                     <ul className = "items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Oleo de cozinha</span>
-                        </li>
+                        {items.map(item => (
+                            <li key = {item.id}>
+                              <img src= {item.image_url} alt={item.name}/>
+                              <span>{item.name}</span>
+                          </li>
+                        ))}
+                      
 
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Oleo de cozinha</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Oleo de cozinha</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Oleo de cozinha</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Oleo de cozinha</span>
-                        </li>
-
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Oleo de cozinha</span>
-                        </li>
                     </ul>
                 </fieldset>
 
